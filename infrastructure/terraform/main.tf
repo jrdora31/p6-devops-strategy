b8609163d7d5756c959a638aca6cfce8a6d03464
JR_Pro <jordan.renoult33@gmail.com>
2026-09-10 14:29:28 +0200

    fix(terraform): use stable keys for NLB targets

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -90,10 +90,10 @@ resource "aws_lb_target_group" "http" {
 }
 
 resource "aws_lb_target_group_attachment" "k3s" {
-  for_each = toset(module.compute.instance_id)
+  for_each = local.instance_roles
 
   target_group_arn = aws_lb_target_group.http.arn
-  target_id        = each.value
+  target_id        = module.compute.instance_id[each.value]
   port             = 80
 }
 
